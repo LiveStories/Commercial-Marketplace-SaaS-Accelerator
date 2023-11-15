03925a5190b6ab766e4cfd6e3b66368396699083
Regis Anclades <regis@anclades.com>
2023-11-15 19:52:28 +0100

    use specified branch as source

--- deployment/Deploy.ps1
+++ deployment/Deploy.ps1
@@ -88,20 +88,6 @@ if(!($KeyVault -match "^[a-zA-Z][a-z0-9-]+$")) {
 
 #endregion 
 
-#region pre-checks
-
-# check if dotnet 6 is installed
-
-$dotnetversion = dotnet --version
-
-if(!$dotnetversion.StartsWith('6.')) {
-    Throw "🛑 Dotnet 6 not installed. Install dotnet6 and re-run the script."
-    Exit
-}
-
-#endregion
-
-
 Write-Host "Starting SaaS Accelerator Deployment..."
 
 #region Select Tenant / Subscription for deployment
@@ -317,8 +303,8 @@ Write-host "   🔵 KeyVault"
 Write-host "      ➡️ Create KeyVault"
 az keyvault create --name $KeyVault --resource-group $ResourceGroupForDeployment --output $azCliOutput
 Write-host "      ➡️ Add Secrets"
-az keyvault secret set --vault-name $KeyVault --name ADApplicationSecret --value="$ADApplicationSecret" --output $azCliOutput
-az keyvault secret set --vault-name $KeyVault --name DefaultConnection --value $Connection --output $azCliOutput
+az keyvault secret set --vault-name $KeyVault  --name ADApplicationSecret --value $ADApplicationSecret --output $azCliOutput
+az keyvault secret set --vault-name $KeyVault  --name DefaultConnection --value $Connection --output $azCliOutput
 
 Write-host "   🔵 App Service Plan"
 Write-host "      ➡️ Create App Service Plan"
